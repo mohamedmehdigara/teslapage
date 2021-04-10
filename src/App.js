f0d6b73b18787849b797185e167b9767bb4ea5c7
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2021-04-10 15:37:02 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
+import Accessories from "./assets/Desktop-Accessories.jpg";
+import ModelS from "./assets/Desktop-ModelS.jpeg";
+import Model3 from "./assets/Desktop-Model3.jpeg";
+import ModelX from "./assets/Desktop-ModelX.jpeg";
+import ModelY from "./assets/Desktop-ModelY.jpeg";
+import SolarPanels from "./assets/Desktop-SolarPanels.jpeg";
+import SolarRoof from "./assets/Desktop-SolarRoof.jpeg";
+import Item from "./components/Item";
 
 function App() {
   return (
     <div className="App">
-      <h1>C</h1>
+      <Header />
+      <div className="app__itemsContainer">
+        <Item
+          title="Lowest Cost Solar Panels in America"
+          desc="Money-back guarantie"
+          descLink=""
+          backgroundImg={SolarPanels}
+          leftBtnTxt="Order Now"
+          leftBtnLink=""
+          twoButtons="true"
+          first
+        />
+        <Item
+          title="ModelS"
+          desc="$69,420"
+          descLink=""
+          backgroundImg={ModelS}
+          leftBtnTxt=" Custom Order"
+          leftBtnLink=""
+          rightBtnTxt="Learn More"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item />
+      </div>
     </div>
   );
 }
